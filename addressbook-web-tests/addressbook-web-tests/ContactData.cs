52a79c0faad7d0b5e398be50ef3a5f771531658b
TumanovPavel <pavel_tymanov@mail.ru>
2020-08-03 12:09:57 +0600

    Сделал всё то же самое, но для контактов

---
+++ addressbook-web-tests/addressbook-web-tests/ContactData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebAddressbookTests
+{
+    class ContactData
+    {
+
+        public string FirstName { set; get; }
+
+        public string LastName { set; get; }
+
+        public ContactData(string firstName)
+        {
+            FirstName = firstName;
+        }
+    }
+}
